bbbaafccdd4acb7f587213bc9011796a0e6f01a2
rachel and colins day with arrays <colinkeith77@gmail.com>
2017-04-04 16:23:42 -0700

    Arrays of fun

---
+++ magicEIGHTball.js
@@ -0,0 +1,9 @@
+
+function tellMeMyFuture() {
+  magicEight["Yup!", "Fuhgeddaboudit", "Maybe", "What would your mother do?", "What would an Australian do? Then do the opposite.", "Sell your home and move to Vegas."]
+prompt("What would you like to know?")
+
+
+  alert(magicEight[Math.floor(Math.random()*6)]
+)}
+tellMeMyFuture()
